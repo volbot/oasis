66cfae4241061ea91768a9af684d7e13d431a390
volbot <lucasmcdermitt@icloud.com>
2023-01-02 23:51:07 -0500

    finally fixed camera for good

--- src/terrain.rs
+++ src/terrain.rs
@@ -15,26 +15,24 @@ impl Terrain {
     }
 
     pub fn draw(&self, cam: &Camera, a: &Assets) {
-        let mut x = cam.corner.0;
-        let mut y = cam.corner.1;
-        println!("{}",x);
-        while x < cam.corner.0 + cam.res.0 as f32 {
-            while y < cam.corner.1 + cam.res.1 as f32 {
-                let tilecoord = (x as isize/40,y as isize/35);
-                if !(tilecoord.0 < 0 || tilecoord.0 >= self.top[0].len() as isize ||
-                     tilecoord.1 < 0 || tilecoord.1 >= self.top.len() as isize) {
-                    let tile = self.top[tilecoord.0 as usize][tilecoord.1 as usize];
+        let mut x = 0;
+        let mut y = 0;
+        while x < self.top[0].len() {
+            while y < self.top.len() {
+                if !(x as isize * 40 < cam.corner.0 as isize - 40 || x > cam.corner.0 as usize + cam.res.0 ||
+                     y as isize * 35 < cam.corner.1 as isize - 35 || y > cam.corner.1 as usize + cam.res.1) {
+                    let tile = self.top[x][y];
                     if tile > 0 {
                         draw_texture(*a.terrain.get(tile-1).unwrap(),
-                        (x - (cam.corner.0 + (cam.corner.0 as isize%40) as f32)) * cam.scale, 
-                        (y - (cam.corner.1 + (cam.corner.1 as isize%35) as f32)) * cam.scale, 
+                        (x * 40) as f32 * cam.scale - cam.corner.0, 
+                        (y * 35) as f32 * cam.scale - cam.corner.1, 
                         WHITE);
                     }
                 }
-                y += 35.;
+                y += 1;
             }
-            x += 40.;
-            y = cam.corner.1;
+            x += 1;
+            y = 0;
         }
     }
 }
